8f89e9c946aaa5cd482750903f37f43365c4eab0
codeddave <drizzzydave@gmail.com>
2021-12-25 17:07:51 +0100

    morgan pkg to log api requests

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorHandlerMiddleware");
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(morgan("tiny"));
 
 const PORT = process.env.PORT || 5000;
 
