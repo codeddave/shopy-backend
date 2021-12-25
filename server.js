063a33cc9dcaad8b9bfb1e8dd7d06b0d044bad29
codeddave <drizzzydave@gmail.com>
2021-12-25 10:19:09 +0100

    updates

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
+
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(cors());
