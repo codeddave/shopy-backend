05ea3239e20cde7edbeae1d5e2bc5f924b55f64d
codeddave <drizzzydave@gmail.com>
2021-12-25 23:49:54 +0100

    Category model, user Routes

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorHandlerMiddleware");
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(morgan("tiny"));
+
+app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
