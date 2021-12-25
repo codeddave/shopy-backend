1b64c1c8999626e7a53d92785ffab6ef54a6946a
codeddave <drizzzydave@gmail.com>
2021-12-25 14:39:33 +0100

    updated entry file,  added error middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
 const cors = require("cors");
+const errorHandler = require("./middleware/errorHandlerMiddleware");
 const app = express();
 
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -12,5 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use("/", (req, res) => {
   res.send("Welcome to the Shopy Api");
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`app started on port ${PORT}`));
