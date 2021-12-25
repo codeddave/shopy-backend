9c4dda24bebecd5ec5e609be01efb7cace6a0e1b
codeddave <drizzzydave@gmail.com>
2021-12-25 14:25:15 +0100

    httpError model  and errorHandlerMiddleware

---
+++ middleware/errorHandlerMiddleware.js
@@ -0,0 +1,9 @@
+const errorhandler = (error, req, res, next) => {
+  if (req.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured." });
+};
+
+module.exports = errorhandler;
