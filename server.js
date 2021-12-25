f74b331ab11fe6cb6eb8d7a0ea951251b6884137
codeddave <drizzzydave@gmail.com>
2021-12-25 10:26:48 +0100

    run app on port

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const app = express();
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`app started on port ${PORT}`));
