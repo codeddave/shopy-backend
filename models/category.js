ae61c3704e96d638141d915d4f4c2fa8d49cd551
codeddave <drizzzydave@gmail.com>
2021-12-26 23:40:17 +0100

    finished category schema, moving on...

--- models/category.js
+++ models/category.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
-const CategorySchema = new mongoose.Schema({});
+const CategorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  icon: String,
+  image: String,
+  color: String,
+});
 
 module.exports = mongoose.model("Category", CategorySchema);
