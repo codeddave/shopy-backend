60293c50c4cbcde7a867f3472da289abcef462ac
codeddave <drizzzydave@gmail.com>
2021-12-26 13:57:23 +0100

    updated product Schema to contain reference to the Category collection

--- models/product.js
+++ models/product.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
+  images: [String],
   description: { type: String, required: true },
   richDescription: { type: String, default: "" },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+
   numberInStock: Number,
 });
 
