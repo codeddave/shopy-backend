0d69d56277c575969a3606dcedfff61603e7f5f4
codeddave <drizzzydave@gmail.com>
2021-12-26 13:59:51 +0100

    product schema update

--- models/product.js
+++ models/product.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  price: { type: String, required: true },
+
   image: { type: String, required: true },
   images: [String],
   description: { type: String, required: true },
   richDescription: { type: String, default: "" },
   category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
 
+  brand: { type: String, default: "" },
   numberInStock: Number,
 });
 
