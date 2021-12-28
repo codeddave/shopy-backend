ebd932aed33385cfcbdd67340f6ac855b970e0af
codeddave <drizzzydave@gmail.com>
2021-12-28 20:40:47 +0100

    created createCategory contoller

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -1,16 +1,30 @@
 const HttpError = require("../models/httpError");
 const Category = require("../models/category");
 
-const createCategory = (req, res, next) => {};
+const createCategory = async (req, res, next) => {
+  const { name, color, image } = req.body;
+  try {
+    const category = await Category.create({
+      name,
+      color,
+      image,
+    });
+    await category.save();
+    res.status(201).json(category);
+  } catch (error) {
+    return next(new HttpError(error.message, 409));
+  }
+};
 
 const getCategories = async (req, res, next) => {
   try {
     const categories = await Category.find();
 
-    res.status(200).json({ categories });
+    res.status(200).json(categories);
   } catch (error) {
     return next(
-      new HttpError("Could not get categories, try again later", 404)
+      new HttpError(error.message, 404)
+      // new HttpError("Could not get categories, try again later", 404)
     );
   }
 };
