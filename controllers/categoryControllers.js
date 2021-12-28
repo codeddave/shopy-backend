bd2a5876e8b9bf8435c5524634e60e45ab5cafc4
codeddave <drizzzydave@gmail.com>
2021-12-28 13:54:31 +0100

    updates to category contollers, getCategory

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -1,3 +1,14 @@
+const Category = require("../models/category");
+
 const createCategory = (req, res, next) => {};
 
+const getCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+
+    res.status(200).json({ categories });
+  } catch (error) {}
+};
+
 exports.createCategory = createCategory;
+exports.getCategories = getCategories;
