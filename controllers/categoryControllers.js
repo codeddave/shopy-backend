52cec2d4b5166a711a781b990357564038141f19
codeddave <drizzzydave@gmail.com>
2021-12-28 15:58:16 +0100

    bring in error mod

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -1,3 +1,4 @@
+const HttpError = require("../models/httpError");
 const Category = require("../models/category");
 
 const createCategory = (req, res, next) => {};
@@ -7,7 +8,11 @@ const getCategories = async (req, res, next) => {
     const categories = await Category.find();
 
     res.status(200).json({ categories });
-  } catch (error) {}
+  } catch (error) {
+    return next(
+      new HttpError("Could not get categories, try again later", 404)
+    );
+  }
 };
 
 exports.createCategory = createCategory;
