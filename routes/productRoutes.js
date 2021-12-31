0b4dbf1c9d326527f825a22b31026b6ec0f298c8
codeddave <drizzzydave@gmail.com>
2021-12-31 14:30:32 +0100

    updated product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,5 +4,8 @@ const productControllers = require("../controllers/productControllers");
 const router = express.Router();
 
 router.get("/", productControllers.getProducts);
+router.post("/", productControllers.createProduct);
+
+router.delete("/:id", productControllers.deleteProduct);
 
 module.exports = router;
