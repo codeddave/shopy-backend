798f0897ff156abab9542ef1b4328f0c00fdfccd
codeddave <drizzzydave@gmail.com>
2021-12-31 02:07:22 +0100

    created updatedCategory contoller

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -1,5 +1,6 @@
 const HttpError = require("../models/httpError");
 const Category = require("../models/category");
+const mongoose = require("mongoose");
 
 const createCategory = async (req, res, next) => {
   const { name, color, image } = req.body;
@@ -55,8 +56,32 @@ const deleteCategory = async (req, res, next) => {
     return next(new HttpError(error.message, 400));
   }
 };
+const updateCategory = async (req, res, next) => {
+  const { id: _id } = req.params;
+  const { name, icon, color } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("No category with that id");
+  }
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(
+      _id,
+      {
+        name,
+        icon,
+        color,
+      },
+      { new: true }
+    );
+
+    res.status(201).json(updatedCategory);
+  } catch (error) {
+    return next(new HttpError(error.message, 409));
+  }
+};
 
 exports.createCategory = createCategory;
 exports.getCategories = getCategories;
 exports.deleteCategory = deleteCategory;
 exports.getCategory = getCategory;
+exports.updateCategory = updateCategory;
