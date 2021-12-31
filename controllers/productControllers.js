d0b6d06e26bfdb77ab95bb0330c77defea461cb2
codeddave <drizzzydave@gmail.com>
2021-12-31 10:25:37 +0100

    updates to the createproduct controller

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Category = require("../models/category");
 const HttpError = require("../models/httpError");
 
 const getProducts = async (req, res, next) => {
@@ -11,8 +12,38 @@ const getProducts = async (req, res, next) => {
 };
 
 const createProduct = async (req, res, next) => {
-  const {} = req.body;
+  const {
+    name,
+    price,
+    image,
+    images,
+    description,
+    richDescription,
+    categoryId,
+    brand,
+    numberInStock,
+    rating,
+    reviewsNumber,
+    isFeatured,
+  } = req.body;
+
   try {
+    const category = await Category.findById(categoryId);
+    if (!category) return next(new HttpError("No category with that Id", 404));
+    await Product.create({
+      name,
+      price,
+      image,
+      images,
+      description,
+      richDescription,
+      category: categoryId,
+      brand,
+      numberInStock,
+      rating,
+      reviewsNumber,
+      isFeatured,
+    });
   } catch (error) {}
 };
 
