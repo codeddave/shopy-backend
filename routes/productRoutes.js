13544e411d9ad1a541c03a0ca611e227f1a9e957
codeddave <drizzzydave@gmail.com>
2022-01-01 15:40:58 +0100

    controller to get featured products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get("/", productControllers.getProducts);
 router.get("/:id", productControllers.getProduct);
 router.get("/count", productControllers.getProductCount);
+router.get("/featured", productControllers.getFeaturedproducts);
 
 router.post("/", productControllers.createProduct);
 
