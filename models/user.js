4d9bed73be0f7d56c5901b55580342f4a4b5fd78
codeddave <drizzzydave@gmail.com>
2022-01-02 23:24:02 +0100

    updates to user schema

--- models/user.js
+++ models/user.js
@@ -2,6 +2,17 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  pasword: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minLength: 6,
+    select: false,
+  },
   phone: Number,
 });
 
