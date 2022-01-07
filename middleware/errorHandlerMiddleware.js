52ad8fbab2e26b60c0b36fa3124a77c76892c708
codeddave <drizzzydave@gmail.com>
2022-01-07 12:51:34 +0100

    updates to error handler

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -2,6 +2,13 @@ const errorHandler = (error, req, res, next) => {
   if (req.headerSent) {
     return next(error);
   }
+  /*  if(error.name === "UnauthorizedError"){
+    return res.status(401).json({message: "The user is not authorized"})
+  }
+
+  if(error.name === "ValicationError"){
+    return res.status(401).json({message: error})
+  } */
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occured." });
 };
