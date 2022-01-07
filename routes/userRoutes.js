c8eb64599dc071785197838eb48c26ca2dc62c80
codeddave <drizzzydave@gmail.com>
2022-01-07 07:30:36 +0100

    added user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const userControllers = require("../controllers/userControllers");
 
 const router = express.Router();
+
+router.get("/", userControllers.getUsers);
+router.get("/:id", userControllers);
+router.post("/register", userControllers.register);
+router.post("/login", userControllers.login);
 
 module.exports = router;
