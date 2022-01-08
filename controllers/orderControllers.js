f389cac7d2686f215bea67af7708e06583c0f996
codeddave <drizzzydave@gmail.com>
2022-01-09 00:40:13 +0100

    added comments for other possibilities

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -5,8 +5,15 @@ const OrderItem = require("../models/orderItem");
 
 const getOrders = async (req, res, next) => {
   try {
-    const orders = await Order.find();
+    const orders = await Order.find().populate("user", "name");
 
+    //get just namme of user
+    //const orders = await Order.find().populate("user", 'name');
+    //sort orders by date
+    //const orders = await Order.find().populate("user", 'name').sort('dateOrdered);
+
+    //sort from newest to oldest
+    //const orders = await Order.find().populate("user", 'name').sort({'dateOrdered: -1});
     res.status(200).json(orders);
   } catch (error) {
     return next(new HttpError(error.message, 404));
