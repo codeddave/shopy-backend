b765eed2a13cf374814b6895e04fd3f2b330d0d2
codeddave <drizzzydave@gmail.com>
2022-01-08 10:15:39 +0100

    updated order schema

--- models/order.js
+++ models/order.js
@@ -1,5 +1,28 @@
 const mongoose = require("mongoose");
 
-const OrderSchema = new mongoose.Schema({});
+const OrderSchema = new mongoose.Schema({
+  orderItems: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+
+      ref: "OrderItem",
+      required: true,
+    },
+  ],
+  shippingAddress1: {
+    type: String,
+    required: true,
+  },
+  shippingAddress2: {
+    type: String,
+    required: true,
+  },
+
+  street: { type: String, required: true },
+  //apartment: { type: String, required: true },
+  zip: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+});
 
 module.exports = mongoose.model("Order", OrderSchema);
