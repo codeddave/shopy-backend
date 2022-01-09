70756214f8e1961700dbf8c706cde314441be02f
codeddave <drizzzydave@gmail.com>
2022-01-09 02:45:15 +0100

    added order routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
-//const productControllers = require("../controllers/productControllers");
+const orderControllers = require("../controllers/orderControllers");
 
 const router = express.Router();
+
+router.get("/", orderControllers.getOrders);
+router.get("/:id", orderControllers.getOrder);
+router.post("/", orderControllers.createOrder);
 
 module.exports = router;
