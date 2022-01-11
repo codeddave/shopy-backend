b4df283b6f4e8624cb2fd64a676411a74970c61d
codeddave <drizzzydave@gmail.com>
2022-01-11 22:19:35 +0100

    updated order routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 router.get("/", orderControllers.getOrders);
 router.get("/:id", orderControllers.getOrder);
+router.get("/count", orderControllers.getOrderCount);
+router.get("/total-sales", orderControllers.getTotalSales);
+
+router.post("/", orderControllers.createOrder);
 router.post("/", orderControllers.createOrder);
 
 module.exports = router;
