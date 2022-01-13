b786480a53b475ef3864188d0b3e6dc0d1099ec4
codeddave <drizzzydave@gmail.com>
2022-01-13 22:32:29 +0100

    hanling file uploads with multer

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const productControllers = require("../controllers/productControllers");
-
 const router = express.Router();
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/", productControllers.getProducts);
 router.get("/:id", productControllers.getProduct);
