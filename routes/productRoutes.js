a280d4378e9cb439b211bcc61d445a661afa45a0
codeddave <drizzzydave@gmail.com>
2022-01-15 19:48:08 +0100

    specifying allowed file extensions for image uploads

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,12 +3,18 @@ const productControllers = require("../controllers/productControllers");
 const router = express.Router();
 const multer = require("multer");
 
+const FILE_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/uploads");
   },
   filename: function (req, file, cb) {
     const fileName = file.originalname.replace(" ", "-");
+    const extension = FILE_TYPE_MAP[file.mimetype];
     cb(null, `${fileName}-${Date.now()}.${extension}`);
   },
 });
