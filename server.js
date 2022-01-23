2446f26e925a3ff1a555946cbc1f1390f49c8350
codeddave <drizzzydave@gmail.com>
2022-01-23 01:56:27 +0100

    specify public/uploads as a static paths

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(morgan("tiny"));
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 
-app.use("/user", userRoutes);
+app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
