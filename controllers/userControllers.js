d691e5d022c5b02562bfff05e4777822c1001257
codeddave <drizzzydave@gmail.com>
2022-03-12 21:59:35 +0100

    updates

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -79,7 +79,7 @@ const login = (async = (req, res, next) => {
 
 const getUsers = async (req, res, next) => {
   try {
-    const users = await User.find();
+    const users = await User.find().select("-password");
 
     res.status(200).json(users);
   } catch (error) {
