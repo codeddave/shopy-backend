92a8d687f1f10b73318313d85767a8fcc2c639bd
codeddave <drizzzydave@gmail.com>
2022-06-06 11:15:02 +0100

    updaetes

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -3,13 +3,13 @@ const errorHandler = (error, req, res, next) => {
     return next(error);
   }
   //Jwt auth error
-  /*  if(error.name === "UnauthorizedError"){
-    return res.status(401).json({message: "The user is not authorized"})
+  if (error.name === "UnauthorizedError") {
+    return res.status(401).json({ message: "The user is not authorized" });
   }
 
-  if(error.name === "ValidationError"){
-    return res.status(401).json({message: error})
-  } */
+  if (error.name === "ValidationError") {
+    return res.status(401).json({ message: error });
+  }
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occured." });
 };
